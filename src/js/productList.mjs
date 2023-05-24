61174c81189527813d22989b01c7641bac51dd80
lechavezl <luisernestochavezleal@gmail.com>
2023-05-23 23:05:48 -0400

    Added dynamic header and footer for the home page

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -32,60 +32,6 @@ export default async function productList(selector, category) {
     fourProducts.push(products[i])
     }
 
-    // console.log(fourProducts);
-    // for (let i = 0; i < 1; i++) {
-    //     products.forEach(tent => {
-    //         console.log(tent);
-    //     });
-    // }
-
-    // products.forEach(tent => {
-    //     for (i = 0; i < 5; i++)
-    //     fourProducts.push(tent[i]);
-    // });
-
-    // console.log(products);
-
-    // for (let i = 0; i < products.length; i++) {
-    //     fourProducts.push(products[i])
-    // }
-
-    // console.log(fourProducts);
-
-    // console.log(products);
-    
     // render out the product list to the element
     renderListWithTemplate(productCardTemplate, el, fourProducts);
-
-    // const tentsData = await response.json();
-    // productCardTemplate(tentsData);
-    // render out the product list to the element
-    // const htmlItems = tentsData.map((product) => productCardTemplate(product));
-    // section.innerHTML = htmlItems.join("");
 }
-
-// async function applyDiscount() {
-//   // const productPriceElements = document.getElementsByClassName('product-card__price');
-//   // const discountedPriceElements = document.getElementsByClassName('discounted-price');
-
-//   const dataProducts = await getData();
-//   // console.log(dataProducts);
-//   let normalPrices = [];
-
-//   dataProducts.forEach(product => {
-//     console.log(product.FinalPrice);    
-//   });
-
-//     if (!isNaN(priceNumber)) {
-//       // Apply the discount and format the result
-//       const discountedPrice = priceNumber * 0.8; // Apply 20% discount
-//       const formattedDiscountedPrice = priceText.replace(priceNumber.toFixed(2), discountedPrice.toFixed(2));
-//       return formattedDiscountedPrice;
-
-//       // Update the discounted-price element
-//       // discountedPriceElements[i].textContent = formattedDiscountedPrice;
-//   }
-// }
-
-// console.log(applyDiscount);
-// applyDiscount()
