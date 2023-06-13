aad192072c857a2a4cee7f080ce80a065a4c9db0
lechavezl <luisernestochavezleal@gmail.com>
2023-06-13 13:13:33 -0400

    Added romove button for the cart page

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -11,6 +11,7 @@ export default function ShoppingCart() {
   // console.log(cartItems)
   renderListWithTemplate(cartItemTemplate, outputEl, cartItems);
   showTotalPrice();
+  removeProduct()
 //   updateCartCount();
     // const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     // document.querySelector(".product-list").innerHTML = htmlItems.join("");
@@ -32,6 +33,7 @@ export default function ShoppingCart() {
     // setLocalStorage("so-cart-prices", pricesLocalStorage);
   
     const newItem = `<li class="cart-card divider">
+    <div class="remove_button" data-id="${item.Id}">X</div>
     <a href="#" class="cart-card__image">
       <img
         src="${item.Images.PrimarySmall}"
@@ -61,39 +63,67 @@ export default function ShoppingCart() {
 //     updateCartCount();
 //   };
   
-  function showTotalPrice() {
-    let checkItems = getLocalStorage("so-cart");
-    const total = document.querySelector(".cart-total")
-    const showTotal = document.querySelector(".hide")
-    
-    let prices = [] // An array to store all the discounted prices.
-    let totalPrice = 0;
+function showTotalPrice() {
+  let checkItems = getLocalStorage("so-cart");
+  const total = document.querySelector(".cart-total")
+  const showTotal = document.querySelector(".hide")
   
-    //Get all the discounted prices in the DOM
-    const discountedPrices = document.getElementsByClassName("cart-card__discounted-price");
-    
-    // Use a for loop to convert the prices to float numbers
-    for (let i = 0; i < discountedPrices.length; i++) {
-      let priceText = discountedPrices[i].textContent; // Get the text E.G ($100.00)
-      let priceNumber = parseFloat(priceText.replace("$", "")); // Convert the text to float
-      prices.push(priceNumber); // Store the discounted prices in an array.
-      setLocalStorage("so-cart-prices", prices);
+  let prices = [] // An array to store all the discounted prices.
+  let totalPrice = 0;
+
+  //Get all the discounted prices in the DOM
+  const discountedPrices = document.getElementsByClassName("cart-card__discounted-price");
   
-    }
-  
-    //Check if there is anything in the cart
-    if (checkItems && checkItems.length > 0) {
-      
-      // Use forEach method to calculate the Total Price
-      prices.forEach(price => {
-        totalPrice += price;
-      })
-  
-      showTotal.style.display = "";
-      total.textContent = `Total: $${totalPrice.toFixed(2)}`;
-      
-      } else {
-        showTotal.style.display = "none"; // If there is no items in the cart, then we don't
-                                          // need to show the Total. 
-      }
+  // Use a for loop to convert the prices to float numbers
+  for (let i = 0; i < discountedPrices.length; i++) {
+    let priceText = discountedPrices[i].textContent; // Get the text E.G ($100.00)
+    let priceNumber = parseFloat(priceText.replace("$", "")); // Convert the text to float
+    prices.push(priceNumber); // Store the discounted prices in an array.
+    setLocalStorage("so-cart-prices", prices);
+
   }
+
+  //Check if there is anything in the cart
+  if (checkItems && checkItems.length > 0) {
+    
+    // Use forEach method to calculate the Total Price
+    prices.forEach(price => {
+      totalPrice += price;
+    })
+
+    showTotal.style.display = "";
+    total.textContent = `Total: $${totalPrice.toFixed(2)}`;
+    
+    } else {
+      showTotal.style.display = "none"; // If there is no items in the cart, then we don't
+                                        // need to show the Total. 
+    }
+}
+
+function removeProduct() {
+  const removeButtons = document.querySelectorAll(".remove_button");
+
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const dataId = button.dataset.id;
+      console.log(dataId);
+      
+      // Get elements form localStorage
+      const cartItems = getLocalStorage("so-cart");
+
+      // Find the index of the element we want to remove
+      const index = cartItems.findIndex((item) => item.Id === dataId);
+
+      if (index !== -1) {
+        // remove the element selected
+        cartItems.splice(index, 1);
+
+        // Store the uptdated list of items
+        setLocalStorage("so-cart", cartItems);
+
+        //Call shoppingCart to render again the function
+        ShoppingCart()
+      }
+    });
+  });
+}
