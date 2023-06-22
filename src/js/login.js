cae8125f52846ef65b278fd91ff859d47490414a
Your Name <you@example.com>
2023-06-21 21:08:10 -0400

    authentication

---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+import {loadHeaderFooter, getParams} from "./utils.mjs"
+loadHeaderFooter()
+
+
+const productId = getParams("redirect");
+
+const login = document.getElementById("loginBtn").addEventListener('click', function() {
+    
+})
+
+
+
